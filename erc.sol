f74ded26ed3c41cb27bbe9af509abd5431eb477e
Muhammad-AhsanTariq <106869223+Muhammad-AhsanTariq@users.noreply.github.com>
2022-08-03 00:43:51 +0500

    Update erc.sol

--- erc.sol
+++ erc.sol
@@ -13,7 +13,7 @@ contract Token  {
     string public symbol = "IT";
     uint public InitialPrice = 1 ether;
     event Transfer(address from , address to , uint value);
-   event Approve(address owner,address spender,uint value);
+    event Approve(address owner,address spender,uint value);
   
     constructor () {
       owner = msg.sender;
@@ -22,28 +22,6 @@ contract Token  {
         require(owner==msg.sender, "Unauthorized User");
         _;
     }
-     /**
-     * Internal transfer, only can be called by this contract
-     */
-    function _transfer(address _from, address _to, uint _value) internal {
-        // Prevent transfer to 0x0 address. Use burn() instead
-        require(_to != address(0x0));
-        // Check if the sender has enough
-        require(balanceOf[_from] >= _value);
-        // Check for overflows
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        // Subtract from the sender
-        balanceOf[_from] -= _value;
-        // Add the same to the recipient
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        // Asserts are used to use static analysis to find bugs in your code. They should never fail
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    
     
     function transfer(address recipient, uint amount) external onlyme returns(bool){
       
@@ -64,7 +42,7 @@ contract Token  {
       return true;
     }
 
-    function transferFrom(address from,address recipient,uint amount) external returns(bool){
+    function transferFrom(address from,address recipient,uint amount) internal returns(bool){
       
       require(msg.sender==spender,"only spender allowed");
       allowance[owner][from] = amount;
@@ -95,10 +73,7 @@ contract Token  {
       function buy() public  payable {
       require (msg.sender != owner, "owner not allowed");
       require(msg.value == InitialPrice, "Please send more ether");
-      _transfer(owner,msg.sender,1);
-      payable(owner).transfer(msg.value);
-
-      
-    
+      transferFrom(owner,msg.sender,1);
+      payable(owner).transfer(msg.value); 
 }
 }
